217e3dcaa856889c1a0be8299c572c2e02587982
Kimikhwan <moonfgg@naver.com>
2025-02-05 00:01:59 +0900

    도감 시작 화면 기능 완료, 몬스터 도감 미완성

---
+++ Assets/Scripts/UI/MonsterUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterUI : BaseUI
+{
+    public override void SetInfo(BaseUIData uiData)
+    {
+        base.SetInfo(uiData);
+    }
+}
