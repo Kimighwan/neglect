23b96e790f4fe33f8998082de61575e0bc14ed92
Kimikhwan <moonfgg@naver.com>
2025-02-04 21:12:03 +0900

    인코딩 방식 수정, Main 씬 UI 오브젝트들 추가, UI 스크립트는 아직 미완성

--- Assets/Scripts/UI/MainUIController.cs
+++ Assets/Scripts/UI/MainUIController.cs
@@ -1,15 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Buffers.Text;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class MainUIController : MonoBehaviour
 {
+    public GameObject encyclopedia;
+    public GameObject blackImage;
+
+    public Transform UICanvasTrs;   // Canvas 위치 닫힌 UI
+    public Transform OpenUITrs;     // 열린 UI
+
+
+    private GameObject frontUI;
+
     void Start()
     {
         Fade.Instance.DoFade(Color.black, 1f, 0f, 1f, 0f, true, false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
 
+        }
+    }
+
+    public void OnClickEncyclopediaBtn()
+    {
+        encyclopedia.SetActive(true);
+        OpenUI();
+    }
+
+    private void OpenUI()
+    {
+        SetBlackImage(true);
+        transform.SetParent(OpenUITrs);
+    }
+
+    private void ClosedUI()
+    {
+        SetBlackImage(false);
+        transform.SetParent(UICanvasTrs);
+    }
+
+    private void SetBlackImage(bool active)
+    {
+        if (active)
+        {
+            blackImage.SetActive(true);
+        }
+        else
+        {
+            blackImage.SetActive(false);
+        }
     }
 }
