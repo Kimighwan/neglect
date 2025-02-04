96f03fb31022d88287875fa4e36925e2f4a97890
Baramini <145976578+Baramini@users.noreply.github.com>
2025-02-04 22:14:55 +0900

    골드, 시간, 날짜 표시 그리고 빨리, 멈춤 버튼

--- Assets/Scripts/Common/GameManager.cs
+++ Assets/Scripts/Common/GameManager.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private bool pause = false;
     void Start()
     {
-        
+        GameInfo.gameInfo.StartGameInfo();
     }
 
     void Update()
     {
-        
+        if (!pause) {
+            GameInfo.gameInfo.UpdateGameInfo();
+            TextHandler.textHandler.UpdateTexts();
+        }
+    }
+
+    public void PauseGame() {
+        pause = !pause;
+        GameInfo.gameInfo.gameSpeed = 1f;
+    }
+    public void QuickGame() {
+        GameInfo.gameInfo.gameSpeed = 4f;
     }
 }
---
+++ Assets/Scripts/Data/GameInfo.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+/*
+게임 진행에 필요한 모든 정보(데이터 베이스 제외)를 사용하려는 목적
+날짜 정보 / 골드 정보 / 시간 정보
+*/
+
+public class GameInfo : MonoBehaviour
+{
+    public static GameInfo gameInfo;
+    // 게임 진행 속도 조절
+    public float gameSpeed = 1f;
+    // 골드 정보
+    private int gold;
+    public int Gold { get { return gold; } set { gold = value; } }
+    // 몇 일차인지
+    private int day;
+    public int Day { get { return day; } set { day = value; } }
+    // 시간 정보
+    private float timer;
+    public float Timer { get { return timer; } set { timer = value; } }
+
+    private void Awake() {
+        gameInfo = this;
+    }
+    public void StartGameInfo() {
+        gold = 10;
+        day = 1;
+        timer = 8.0f;
+    }
+    public void UpdateGameInfo() {
+        timer += Time.deltaTime * gameSpeed;
+        if (timer >= 24f) {
+            timer %= 24f;
+            day += 1;
+        }
+    }
+}
