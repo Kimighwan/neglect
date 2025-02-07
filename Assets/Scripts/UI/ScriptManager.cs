f3567d60727236863e061c3e9bf588c72ce0f942
Baramini <145976578+Baramini@users.noreply.github.com>
2025-02-08 03:53:43 +0900

    인트로 스크립트 적용

--- Assets/Scripts/UI/ScriptManager.cs
+++ Assets/Scripts/UI/ScriptManager.cs
@@ -10,9 +10,11 @@ public class ScriptManager : MonoBehaviour
     //public Image bgImage;
     public GameObject panel;
     public TextMeshProUGUI dialogueText;  // UI에 연결할 Text 오브젝트
+    public TextMeshProUGUI speakerText;
     public float typingSpeed = 0.05f; // 글자 출력 속도 조절 (낮을수록 빠름)
     
     private string[] lines;     // 대사 저장 배열
+    private string s;
     private int currentLine = 0;
     private bool isTyping = false; // 현재 타이핑 중인지 확인
     private Coroutine typingCoroutine;
@@ -20,7 +22,7 @@ public class ScriptManager : MonoBehaviour
     void Start()
     {
         scriptManager = this;
-        TextAsset textAsset = Resources.Load<TextAsset>("TextScripts/TestScript");
+        TextAsset textAsset = Resources.Load<TextAsset>("TextScripts/IntroScript");
         lines = textAsset.text.Split('\n'); // 줄 단위로 나누기
         background.SetActive(true);
         ShowNextDialogue(); // 첫 번째 대사 출력
@@ -35,8 +37,9 @@ public class ScriptManager : MonoBehaviour
             {
                 // 타이핑 도중 클릭하면 즉시 전체 출력
                 StopCoroutine(typingCoroutine);
-                dialogueText.text = lines[currentLine - 1].Trim(); // 현재 대사 전체 표시
+                dialogueText.text = s; // 현재 대사 전체 표시
                 isTyping = false;
+                s = "";
             }
             else
             {
@@ -53,7 +56,15 @@ public class ScriptManager : MonoBehaviour
             {
                 StopCoroutine(typingCoroutine);
             }
-            typingCoroutine = StartCoroutine(TypeText(lines[currentLine].Trim()));
+            s = lines[currentLine].Trim();
+            if (s[0] == '$') {
+                int start = s.IndexOf('{') + 1;
+                int end = s.IndexOf('}');
+                string speaker = s.Substring(start, end - start); // 인물 이름 추출
+                speakerText.text = '「' + speaker + '」'; // UI에 인물 정보 표시
+                s = s.Substring(end + 1); // '}' 다음부터 대사 가져오기
+            } else speakerText.text = "";
+            typingCoroutine = StartCoroutine(TypeText(s));
             currentLine++; // 다음 대사로 이동
         }
         else
