8a38bc621fada689ed28550cbff279073556629d
Kimikhwan <moonfgg@naver.com>
2025-02-12 21:24:17 +0900

    파견에서 모험가 목록 정렬 구현

--- Assets/Scripts/UI/Detach/DetachAdventureListUI.cs
+++ Assets/Scripts/UI/Detach/DetachAdventureListUI.cs
@@ -1,13 +1,23 @@
 using Gpm.Ui;
+using TMPro;
 using UnityEngine;
 
 public class DetachAdventureListUI : BaseUI
 {
     public InfiniteScroll infiniteScrollList;
 
-    public override void Init(Transform anchor)
+    public TextMeshProUGUI sortBtnText;
+    public TextMeshProUGUI orderBtnText;
+
+
+    private AdventureSortType adventureSortType = AdventureSortType.GRADE;
+    private AdventureOrderType adventureOrderType = AdventureOrderType.DOWN;
+
+    public override void SetInfo(BaseUIData uiData)
     {
-        base.Init(anchor);
+        base.SetInfo(uiData);
+
+        SortAdventure();
     }
 
     private void OnEnable()
@@ -32,6 +42,212 @@ public class DetachAdventureListUI : BaseUI
             slotData.adventureTier = adventureData.adventureTier;
 
             infiniteScrollList.InsertData(slotData);
+        }
+    }
+
+    private void SortAdventure()
+    {
+        switch (adventureSortType)
+        {
+            case AdventureSortType.GRADE:
+                sortBtnText.text = "GRADE";
+
+                infiniteScrollList.SortDataList((a, b) =>
+                {
+                    int compareResult = 0;
+
+                    var itemA = a.data as AdventureData;
+                    var itemB = b.data as AdventureData;
+
+                    // 내림차순
+                    if (adventureOrderType == AdventureOrderType.DOWN)
+                    {
+                        compareResult = ((itemB.adventureId / 1000) % 10).CompareTo((itemA.adventureId / 1000) % 10);
+                    }
+
+                    // 오름차순
+                    if (adventureOrderType == AdventureOrderType.UP)
+                    {
+                        compareResult = ((itemA.adventureId / 1000) % 10).CompareTo((itemB.adventureId / 1000) % 10);
+                    }
+
+                    if (compareResult == 0)
+                    {
+                        var itemAName = itemA.adventureName;
+
+                        var itemBName = itemB.adventureName;
+
+                        compareResult = itemAName.CompareTo((itemBName));
+                    }
+
+                    return compareResult;
+                });
+                break;
+            case AdventureSortType.POSITION:
+                sortBtnText.text = "POSITION";
+
+                infiniteScrollList.SortDataList((a, b) =>
+                {
+                    int compareResult = 0;
+
+                    var itemA = a.data as AdventureData;
+                    var itemB = b.data as AdventureData;
+
+                    // 내림차순
+                    if (adventureOrderType == AdventureOrderType.DOWN)
+                    {
+                        compareResult = ((itemB.adventureId / 100) % 10).CompareTo((itemA.adventureId / 100) % 10);
+                    }
+
+                    // 오름차순
+                    if (adventureOrderType == AdventureOrderType.UP)
+                    {
+                        compareResult = ((itemA.adventureId / 100) % 10).CompareTo((itemB.adventureId / 100) % 10);
+                    }
+
+                    if (compareResult == 0)
+                    {
+                        var itemAName = itemA.adventureName;
+
+                        var itemBName = itemB.adventureName;
+
+                        compareResult = itemAName.CompareTo((itemBName));
+                    }
+
+                    return compareResult;
+                });
+                break;
+            case AdventureSortType.CLASS:
+                sortBtnText.text = "CLASS";
+
+                infiniteScrollList.SortDataList((a, b) =>
+                {
+                    int compareResult = 0;
+
+                    var itemA = a.data as AdventureData;
+                    var itemB = b.data as AdventureData;
+
+                    // 내림차순
+                    if (adventureOrderType == AdventureOrderType.DOWN)
+                    {
+                        compareResult = ((itemB.adventureId / 10) % 10).CompareTo((itemA.adventureId / 10) % 10);
+                    }
+
+                    // 오름차순
+                    if (adventureOrderType == AdventureOrderType.UP)
+                    {
+                        compareResult = ((itemA.adventureId / 10) % 10).CompareTo((itemB.adventureId / 10) % 10);
+                    }
+
+                    if (compareResult == 0)
+                    {
+                        var itemAName = itemA.adventureName;
+
+                        var itemBName = itemB.adventureName;
+
+                        compareResult = itemAName.CompareTo((itemBName));
+                    }
+
+                    return compareResult;
+                });
+                break;
+            case AdventureSortType.TYPE:
+                sortBtnText.text = "TYPE";
+
+                infiniteScrollList.SortDataList((a, b) =>
+                {
+                    int compareResult = 0;
+
+                    var itemA = a.data as AdventureData;
+                    var itemB = b.data as AdventureData;
+
+                    // 내림차순
+                    if (adventureOrderType == AdventureOrderType.DOWN)
+                    {
+                        compareResult = (itemB.adventureId % 10).CompareTo(itemA.adventureId % 10);
+                    }
+
+                    // 오름차순
+                    if (adventureOrderType == AdventureOrderType.UP)
+                    {
+                        compareResult = (itemA.adventureId % 10).CompareTo(itemB.adventureId % 10);
+                    }
+
+                    if (compareResult == 0)
+                    {
+                        var itemAName = itemA.adventureName;
+
+                        var itemBName = itemB.adventureName;
+
+                        compareResult = itemAName.CompareTo((itemBName));
+                    }
+
+                    return compareResult;
+                });
+                break;
+            default:
+                break;
+        }
+
+        OrderTextUpdate();
+    }
+
+    // 정렬 버튼
+    public void OnClickSortBtn()
+    {
+        switch (adventureSortType)
+        {
+            case AdventureSortType.GRADE:
+                adventureSortType = AdventureSortType.POSITION;
+                break;
+            case AdventureSortType.POSITION:
+                adventureSortType = AdventureSortType.CLASS;
+                break;
+            case AdventureSortType.CLASS:
+                adventureSortType = AdventureSortType.TYPE;
+                break;
+            case AdventureSortType.TYPE:
+                adventureSortType = AdventureSortType.GRADE;
+                break;
+            default:
+                break;
+        }
+
+        SortAdventure();
+    }
+
+    // 순서 선택 버튼
+    public void OnClickOrderBtn()
+    {
+        switch (adventureOrderType)
+        {
+            case AdventureOrderType.DOWN:
+                adventureOrderType = AdventureOrderType.UP;
+                orderBtnText.text = "UP";
+                break;
+            case AdventureOrderType.UP:
+                adventureOrderType = AdventureOrderType.DOWN;
+                orderBtnText.text = "DOWN";
+                break;
+            default:
+                break;
+        }
+
+        SortAdventure();
+    }
+
+    private void OrderTextUpdate()
+    {
+        switch (adventureOrderType)
+        {
+            case AdventureOrderType.DOWN:
+                orderBtnText.text = "DOWN";
+                break;
+            case AdventureOrderType.UP:
+                orderBtnText.text = "UP";
+                break;
+            default:
+                break;
         }
     }
 }
