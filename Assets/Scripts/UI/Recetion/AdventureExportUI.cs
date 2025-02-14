bf0089edfcd261ccff62005eae09ba7255a58db1
Kimikhwan <moonfgg@naver.com>
2025-02-15 01:29:47 +0900

    모험가 방출 구현 완료, 방출에 따른 골드 얻기만 하면 끝

--- Assets/Scripts/UI/Recetion/AdventureExportUI.cs
+++ Assets/Scripts/UI/Recetion/AdventureExportUI.cs
@@ -1,17 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AdventureExportUI : BaseUI
 {
+
+
     public override void Init(Transform anchor)
     {
         base.Init(anchor);
     }
+
     public void OnClickExportBtn()
     {
+        UIManager.Instance.CloseUI(this);
+
+        Debug.Log("모험가 방출!!");
+        // 제거하는 모험가에 따라 골드 얻기
+        // AddGold()
+
         // PlayerPrefs에 해당 모험가 제거
+        DeleteAdventure();
+
         // 인피니티 스크롤 업데이트
-        Debug.Log("Export!!!");
+        AdventurerListUI.Instance.UpdateScrollItem();
+    }
+
+    private void DeleteAdventure()
+    {
+        var adventureId = PlayerPrefs.GetString("AdventureId");
+        var adventureIds = adventureId.Split(',');
+
+        if (adventureId == "") return;
+
+        string addId = "";
+
+        foreach (var item in adventureIds)
+        {
+            int adventureIdOfInt = Convert.ToInt32(item);
+
+            if (UIManager.Instance.exportAdventureId != adventureIdOfInt)
+            {
+                if (addId == "")
+                    addId += adventureIdOfInt.ToString();
+                else
+                    addId += "," + adventureIdOfInt.ToString();
+            }
+        }
+
+        PlayerPrefs.SetString("AdventureId", addId);
     }
 }
