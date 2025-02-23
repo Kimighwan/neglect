248f5df3d82b463a7dde5c725f64f042d004e227
Baramini <145976578+Baramini@users.noreply.github.com>
2025-02-24 00:40:42 +0900

    UI디자인 적용

--- Assets/Scripts/UI/Request.cs
+++ Assets/Scripts/UI/Request.cs
@@ -9,6 +9,8 @@ public class Request : MonoBehaviour
     private List<TextMeshProUGUI> states = new List<TextMeshProUGUI>();
     private List<Button> quests = new List<Button>();
     private List<Button> adventures = new List<Button>();
+    private List<Button> questStarts = new List<Button>();
+    private List<Button> awakes = new List<Button>();
 
     private List<bool> isActivated = new List<bool>{ true, true, false, false, false };
     private List<bool> isAllocated = new List<bool>{ false, false, false, false, false };
@@ -49,6 +51,16 @@ public class Request : MonoBehaviour
                         TextMeshProUGUI stateComponent = child.GetComponent<TextMeshProUGUI>();
                         if (stateComponent != null) states.Add(stateComponent);
                         break;
+
+                    case "QuestStart":
+                        Button button3 = child.GetComponent<Button>();
+                        if (button3 != null) questStarts.Add(button3);
+                        break;
+                    
+                    case "Awake":
+                        Button button4 = child.GetComponent<Button>();
+                        if (button4 != null) awakes.Add(button4);
+                        break;
                 }
             }
         }
@@ -63,19 +75,26 @@ public class Request : MonoBehaviour
             requests[i].GetComponent<Image>().color = new Color(1f, 1f, 1f);
             quests[i].interactable = true;
             adventures[i].interactable = true;
-            states[i].text = "파견 대기중";
+            awakes[i].interactable = true;
+            states[i].text = "";
             GameInfo.gameInfo.Requests++;
         }
     }
-    public void SetState() {
+    public void SetState() { // 모든 버튼의 상태를 정하는 메소드로 할당이 다 되면 호출, 초기화 시 호출
         for (int i = 0; i < 5; i++) {
             if (!isActivated[i]) {
                 quests[i].interactable = false;
                 adventures[i].interactable = false;
+                questStarts[i].interactable = false;
+                awakes[i].interactable = false;
                 states[i].text = "길드 레벨 ??에 개방";
             }
             else {
-                if (!isAllocated[i]) states[i].text = "파견 대기중";
+                states[i].text = "";
+                if (!isAllocated[i]) {
+                    questStarts[i].interactable = false;
+                }
+                else questStarts[i].interactable = true;
             }
         }
     }
