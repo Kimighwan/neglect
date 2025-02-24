b0104e6da9a6e92cbf1c87ff1d3b33f332bdabf2
Kimikhwan <moonfgg@naver.com>
2025-02-25 03:15:50 +0900

    모험가 방출 UI 다시 만듦, 모험가 Detatil UI  다시 구현

--- Assets/Scripts/UI/Recetion/AdventureListSlotItem.cs
+++ Assets/Scripts/UI/Recetion/AdventureListSlotItem.cs
@@ -2,6 +2,7 @@ using Gpm.Ui;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +10,17 @@ public class AdventureListSlotItem : InfiniteScrollItem
 {
     public TextMeshProUGUI m_name;
 
-    public Image positionPos;
-    public Image classPos;
-    public Image typePos;
-    public Image rank;
+    public RawImage positionImg;
+    public RawImage classImg;
+    public RawImage typeImg;
+    public RawImage rankImg;
 
+    public TextMeshProUGUI nameTxt;
+    public TextMeshProUGUI positionTxt;
+    public TextMeshProUGUI classTxt;
+    public TextMeshProUGUI typeTxt;
+
+    public GameObject exportGameObject;
 
     private AdventureData adventureData;
 
@@ -37,6 +44,47 @@ public class AdventureListSlotItem : InfiniteScrollItem
         var adventureTier = adventureData.adventureTier;
         adventureClass = adventureData.adventureClass;
 
+        nameTxt.text = adventureName;
+        positionTxt.text = adventurePosition;
+        classTxt.text = adventureClass;
+        typeTxt.text = adventureType;
+
+        // Postion Image
+        if (adventurePosition == "전위")
+            positionImg.texture = Resources.Load("Arts/Icon/Pos/FrontIcon") as Texture2D;
+        else if(adventurePosition == "중위")
+            positionImg.texture = Resources.Load("Arts/Icon/Pos/MiddleIcon") as Texture2D;
+        else
+            positionImg.texture = Resources.Load("Arts/Icon/Pos/BackIcon") as Texture2D;
+
+        // Class Image
+        if (adventureClass == "공격")
+            classImg.texture = Resources.Load("Arts/Icon/Class/AttackIcon") as Texture2D;
+        else if (adventurePosition == "방어")
+            classImg.texture = Resources.Load("Arts/Icon/Class/DefenceIcon") as Texture2D;
+        else
+            classImg.texture = Resources.Load("Arts/Icon/Class/SupportIcon") as Texture2D;
+
+        // Type Image
+        if (adventureClass == "물리")
+            typeImg.texture = Resources.Load("Arts/Icon/Type/PhysicIcon") as Texture2D;
+        else if (adventurePosition == "마법")
+            typeImg.texture = Resources.Load("Arts/Icon/Type/MagicIcon") as Texture2D;
+        else
+            typeImg.texture = Resources.Load("Arts/Icon/Type/HolyIcon") as Texture2D;
+
+        // Rank Image
+        if (adventureTier == "브론즈")
+            rankImg.texture = Resources.Load("Arts/Rank/RankBronze") as Texture2D;
+        else if (adventureTier == "실버")
+            rankImg.texture = Resources.Load("Arts/Rank/RankSilver") as Texture2D;
+        else if (adventureTier == "골드")
+            rankImg.texture = Resources.Load("Arts/Rank/RankGold") as Texture2D;
+        else if (adventureTier == "플래티넘")
+            rankImg.texture = Resources.Load("Arts/Rank/RankPlatinum") as Texture2D;
+        else
+            rankImg.texture = Resources.Load("Arts/Rank/RankDiamond") as Texture2D;
+
         m_name.text = adventureName;
     }
 
@@ -47,20 +95,22 @@ public class AdventureListSlotItem : InfiniteScrollItem
     }
 
 
-    public void OpenExportBtnUI()
+    public void OnClickExportGameObjectUI()
     {
-        var adventureExportUI = new BaseUIData();
-        UIManager.Instance.OpenUI<AdventureExportUI>(adventureExportUI);
-
-        var ui = UIManager.Instance.GetActiveUI<AdventureExportUI>();
-
-        ui.gameObject.transform.SetParent(this.transform);
-
-        var rectTransform = ui.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector3(0f, 0f, 0f);
-        rectTransform.sizeDelta = new Vector2(110f, 110f);
+        exportGameObject.SetActive(true);
 
         UIManager.Instance.exportAdventureId = adventureId;
+
+        //var adventureExportUI = new BaseUIData();
+        //UIManager.Instance.OpenUI<AdventureExportUI>(adventureExportUI);
+
+        //var ui = UIManager.Instance.GetActiveUI<AdventureExportUI>();
+
+        //ui.gameObject.transform.SetParent(this.transform);
+
+        //var rectTransform = ui.GetComponent<RectTransform>();
+        //rectTransform.anchoredPosition = new Vector3(0f, 0f, 0f);
+        //rectTransform.sizeDelta = new Vector2(110f, 110f);
     }
 
 
