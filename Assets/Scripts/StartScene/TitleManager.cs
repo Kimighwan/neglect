ed9dfe530ff42781ded4a105122f228cc7124f10
Baramini <145976578+Baramini@users.noreply.github.com>
2025-02-25 23:30:46 +0900

    사운드 추가

--- Assets/Scripts/StartScene/TitleManager.cs
+++ Assets/Scripts/StartScene/TitleManager.cs
@@ -1,31 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
-
     void OnEnable()
     {
-        Invoke("PlayTitleBGM", 0.25f);
+        Invoke("PlayTitleBGM", 0.1f);
     }
 
     private void PlayTitleBGM() {
         if (AudioManager.Instance != null) {
             AudioManager.Instance.UnMute();
-            AudioManager.Instance.ChangeBGMVolume(0.5f);
-            AudioManager.Instance.ChangeSFXVolume(0.5f);
             AudioManager.Instance.PlayBGM(BGM.Start);
         }
-        else Invoke("PlayTitleBGM", 0.25f);
+        else Invoke("PlayTitleBGM", 0.1f);
     }
 }
