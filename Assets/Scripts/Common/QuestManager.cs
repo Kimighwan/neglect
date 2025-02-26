232388a56e40cac46cbf4675e52b20912e831b77
Kimikhwan <moonfgg@naver.com>
2025-02-27 01:52:13 +0900

    모든 파견창 셋팅 완료

--- Assets/Scripts/Common/QuestManager.cs
+++ Assets/Scripts/Common/QuestManager.cs
@@ -61,7 +61,7 @@ public class QuestManager : SingletonBehaviour<QuestManager>
 
     private void Update()
     {
-        for(int i = 0; i < 1; i++)
+        for(int i = 0; i < 5; i++)
         {
             if(questBtn[i].interactable == false && adventureBtn[i].interactable == false)
             {
