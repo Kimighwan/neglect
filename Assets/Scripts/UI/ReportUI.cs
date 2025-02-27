d4ba268ab92ce50a543c001c0c576b3e09e17e80
Baramini <145976578+Baramini@users.noreply.github.com>
2025-02-27 16:21:25 +0900

    아트 리소스 추가 적용

--- Assets/Scripts/UI/ReportUI.cs
+++ Assets/Scripts/UI/ReportUI.cs
@@ -9,16 +9,44 @@ public class ReportUI : BaseUI
     public TextMeshProUGUI quest;
     public TextMeshProUGUI NowScore;
     public TextMeshProUGUI NextScore;
+    public Image StampShadow;
+    public Image Stamp;
+
+    private float shadowDuration = 0.7f;
+    private Vector3 startScale = new Vector3(0.1f, 0.1f, 0.1f);
+    private Vector3 endScale = new Vector3(0.9f, 0.9f, 0.9f);
+    private Color startColor = new Color(0, 0, 0, 0.05f);
+    private Color endColor = new Color(0, 0, 0, 0.9f);
+    private bool stampOn = false;
 
     private void OnEnable() {
         day.text = "";
         quest.text = "";
         NowScore.text = "";
         NextScore.text = "";
+        if (Stamp != null) {
+            Stamp.enabled = false;
+            stampOn = false;
+        }
+        if (StampShadow != null) {
+            StampShadow.enabled = true;
+            StampShadow.rectTransform.localScale = startScale;
+            StampShadow.color = startColor;
+            StampShadow.enabled = false;
+        }
         DoFadeIn();
         Invoke("StartTyping", 0.5f);
         AudioManager.Instance.PlayBGM(BGM.TypeWriter);
     }
+
+    void Update()
+    {
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0)) {
+            if (!stampOn) StartCoroutine(AnimateStampShadow());
+            else OnClickCloseBut();
+        }
+    }
+
     public void OnClickCloseBut()
     {
         image.gameObject.SetActive(true);
@@ -72,38 +100,65 @@ public class ReportUI : BaseUI
         foreach (char letter in s1)
         {
             day.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.025f);
         }
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
 
         quest.text = "";
         foreach (char letter in s2)
         {
             quest.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.025f);
         }
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
 
         NowScore.text = "";
         foreach (char letter in s3)
         {
             NowScore.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.025f);
         }
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
 
         NextScore.text = "";
         foreach (char letter in s4)
         {
             NextScore.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.025f);
         }
 
         AudioManager.Instance.StopBGM();
         AudioManager.Instance.PlaySFX(SFX.CoinDrop1);
         AudioManager.Instance.PlaySFX(SFX.CoinDrop2);
+    }
+
+    IEnumerator AnimateStampShadow()
+    {
+        StampShadow.enabled = true;
+        AudioManager.Instance.PlaySFX(SFX.Stamp);
+        float elapsed = 0f;
+        while (elapsed < shadowDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / shadowDuration);
+
+            // 스케일 및 색상 선형 보간
+            if (StampShadow != null)
+            {
+                StampShadow.rectTransform.localScale = Vector3.Lerp(startScale, endScale, t);
+                StampShadow.color = Color.Lerp(startColor, endColor, t);
+            }
+
+            yield return null;
+        }
+        // 애니메이션 완료 시 스탬프 이미지 활성화
+        if (Stamp != null) {
+            StampShadow.enabled = false;
+            Stamp.enabled = true;
+            stampOn = true;
+        }
     }
 }
