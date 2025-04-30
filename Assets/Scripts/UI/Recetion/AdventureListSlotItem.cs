8b26faee68a70d711d80b4585f15c2fffcd4030f
Kimikhwan <moonfgg@naver.com>
2025-05-01 00:00:13 +0900

    0.2.9

--- Assets/Scripts/UI/Recetion/AdventureListSlotItem.cs
+++ Assets/Scripts/UI/Recetion/AdventureListSlotItem.cs
@@ -1,12 +1,8 @@
 using Gpm.Ui;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
 
 public class AdventureListSlotItem : InfiniteScrollItem
 {
