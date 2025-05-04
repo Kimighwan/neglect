9b864912dff018d83352acec514b819ee26dbcd1
Baramini <145976578+Baramini@users.noreply.github.com>
2025-05-04 22:49:12 +0900

    텍스트 애니메이터 추가 및 현재 테스트 중

--- Assets/Scripts/Text/DialogMode.cs
+++ Assets/Scripts/Text/DialogMode.cs
@@ -1,3 +1,4 @@
+using Febucci.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,7 +10,6 @@ public class DialogMode : MonoBehaviour
     private ScriptDialogObjectData data;
     private float holdTime = 2f;
     private float changeHoldTime = 2f;
-    private bool killDialog = false;
 
     private Queue<ScriptData> scriptQueue = new Queue<ScriptData>();
     private bool isSpeakerA = true;
@@ -25,7 +25,6 @@ public class DialogMode : MonoBehaviour
     public void PrepareDialogText(int startId, int endId)
     {
         onSpeaking = false;
-        killDialog = false;
         scriptQueue.Clear();
         for (int i = startId; i <= endId; i++)
         {
@@ -36,97 +35,67 @@ public class DialogMode : MonoBehaviour
             }
         }
         onSpeaking = true;
-        StartCoroutine(StartDialogue());
+        data.ActiveAllDialogObject(false, false);
+        PlayDialogLine();
     }
 
-    private IEnumerator StartDialogue()
+    public void PlayDialogLine()
     {
-        data.ActiveAllDialogObject(false, false);
-
-        while (scriptQueue.Count > 0)
-        {
+        if (scriptQueue.Count > 0) {
             ScriptData scriptData = scriptQueue.Dequeue();
-
-            StartCoroutine(TypeSentence(scriptData));
-
-            yield return new WaitForSeconds(holdTime);
-
-            while (GameManager.gameManager.Pause) {
-                yield return null;
-            }
-            if (killDialog) {
-                killDialog = false;
-                scriptQueue.Clear();
-                break;
-            }
+            TypeSentence(scriptData);
+            // 출력하고 HoldTime 추가
+            // 퍼즈하면 재생되다가 멈추도록
+            // killDialog뜨면 그 상태로 끝내야함
         }
-
-        data.ActiveAllDialogObject(false, false);
-        onSpeaking = false;
+        // while (scriptQueue.Count > 0)
+        // {
+        //     yield return new WaitForSeconds(holdTime);
+        //     while (GameManager.gameManager.Pause) {
+        //         yield return null;
+        //     }
+        // }
+        else {
+            onSpeaking = false;
+            data.ActiveAllDialogObject(false, false);
+        }
     }
 
-    private IEnumerator TypeSentence(ScriptData scriptData)
+    private void TypeSentence(ScriptData scriptData)
     {
+        if (scriptData.scriptSpeaker == "NPC1") isSpeakerA = true;
+        else isSpeakerA = false;
+
         if (isSpeakerA)
         {
             data.ActiveAllDialogObject(true, false);
-            data.malpungseon1Text.text = "";
+            data.malpungseon1Text.GetComponent<TypewriterByCharacter>().ShowText(scriptData.scriptLine);
             data.speaker1Text.text = scriptData.scriptSpeaker;
-            yield return StartCoroutine(TypeEffect(data.malpungseon1Text, scriptData.scriptLine, data.malpungseonUI1, data.malpungseon1));
         }
         else
         {
             data.ActiveAllDialogObject(false, true);
-            data.malpungseon2Text.text = "";
+            data.malpungseon2Text.GetComponent<TypewriterByCharacter>().ShowText(scriptData.scriptLine);
             data.speaker2Text.text = scriptData.scriptSpeaker;
-            yield return StartCoroutine(TypeEffect(data.malpungseon2Text, scriptData.scriptLine, data.malpungseonUI2, data.malpungseon2));
         }
-        isSpeakerA = !isSpeakerA;
     }
 
-    // 수정된 부분: 전체 문장을 즉시 출력
-    private IEnumerator TypeEffect(TextMeshProUGUI targetText, string sentence, GameObject targetUI, GameObject targetMalpungseon) {
-        targetText.text = sentence;
-
-        // Vector3 originalUIPos = targetUI.transform.localPosition;
-        // Vector3 originalPosMal = targetMalpungseon.transform.position;
-
-        // int shakeCount = 3;
-        // float period = 0.5f;
-        // float uiOffsetY = 5f;
-        // float offsetY = 0.0741f;
-
-        // for (int i = 0; i < shakeCount; i++)
-        // {
-        //     float elapsed = 0f;
-            
-        //     while (elapsed < period)
-        //     {
-        //         float t = elapsed / period;
-
-        //         // UI는 local position에 오프셋을 더해 업데이트
-        //         targetUI.transform.localPosition = originalUIPos + new Vector3(0, uiOffsetY, 0);
-
-        //         // UI offset을 world space로 변환하여 말풍선에도 적용
-        //         Vector3 worldOffset = targetUI.transform.TransformVector(new Vector3(0, offsetY, 0));
-        //         targetMalpungseon.transform.position = originalPosMal + worldOffset;
-
-        //         elapsed += Time.deltaTime;
-        //         yield return null;
-        //     }
-        //     uiOffsetY *= -1;
-        //     offsetY *= -1;
-        // }
-
-        // // 움직임 종료 후 원래 위치로 복원
-        // targetUI.transform.localPosition = originalUIPos;
-        // targetMalpungseon.transform.position = originalPosMal;
-
-        yield return null;
+    public void SkipTypingAnimation() {
+        if (isSpeakerA)
+        {
+            data.malpungseon1Text.GetComponent<TypewriterByCharacter>().StartDisappearingText();
+            data.malpungseon1Text.GetComponent<TypewriterByCharacter>().SkipTypewriter();
+        }
+        else
+        {
+            data.malpungseon2Text.GetComponent<TypewriterByCharacter>().StartDisappearingText();
+            data.malpungseon2Text.GetComponent<TypewriterByCharacter>().SkipTypewriter();
+        }
+        PlayDialogLine();
     }
 
     public void KillDialog() {
-        killDialog = true;
+        scriptQueue.Clear();
     }
     public bool IsSpeaking() {
         return onSpeaking;
